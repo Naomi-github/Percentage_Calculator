7ee9f2c0e2631ae6f5ee54683937503fec50c8b8
Naomi Starr <naomi.starr.mail@gmail.com>
2023-05-09 11:53:17 +0100

    variable assigned in MainActivity.kt

--- app/src/main/java/com/starr/percentage_calculator/MainActivity.kt
+++ app/src/main/java/com/starr/percentage_calculator/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.starr.percentage_calculator
+import android.util.Log;
+
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 
+private const val TAG = "MainActivity"
+private const val INITIAL_TIP_PERCENT = 15
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvPercentageLabel: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarTip)
+        tvPercentageLabel = findViewById(R.id.tvPercentageLabel)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+        seekBarTip.progress = INITIAL_TIP_PERCENT
+        tvPercentageLabel.text = "$INITIAL_TIP_PERCENT%"
+
+
+        seekBarTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Log.i(TAG, "onProgressChanged $progress")
+                tvPercentageLabel.text = "$progress%"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+
+        })
     }
 }
